a98e436324abb41982de47ff6f41020b7b3d914a
Mahe081328 <mahendran081328@gmail.com>
2025-12-09 12:29:17 +0530

    Enhance Calc.py with comments for better understanding
    Added comments for clarity on class usage and loop behavior.

--- Calc.py
+++ Calc.py
@@ -1,4 +1,4 @@
-class Opp:
+class Opp: #Using class so that it can be used in other files.
     def add(a, b):
         return a+b
     
@@ -11,15 +11,15 @@ class Opp:
     def div(a, b):
         return a/b
 
-while True:
+while True: #Looping endless to perform as many calculations as you want.  
     print("Choose Operation")
     print("1. ADD \t 2. SUB \t 3. MUL \t 4. DIV \t 5. EXIT ")
     ch = int(input("Enter your choice as 1/2/3/4/5 : "))
     if (ch < 1 or ch > 5):
         print("Enter a Valid Choice!! \n")
-        continue
+        continue #Will ask to choose again after a wrong input.
     elif (ch == 5):
-        break
+        break #Will end the loop upon choosing 5
     else:
         print()
         num1 = int(input("Enter Your First Number: "))
@@ -34,6 +34,7 @@ while True:
             print(f"Product of {num1} and {num2} is: ", Opp.mul(num1, num2))
             print(" ")
         elif (ch == 4):
+            #Exception Handling for division by zero.
             if num2 == 0:
                 try: 
                     num1/num2
